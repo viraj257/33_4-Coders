4cfb6ee9d027b3d207241ab6c208d7a30c24993e
viraj257 <virajtaywade@gmail.com>
2022-10-07 23:23:20 +0530

    included footer

---
+++ includes/footer.php
@@ -0,0 +1,48 @@
+<footer class="bg-dark text-center text-white">
+    <div class="container py-4">
+            <div class="row gy-4 gx-5">
+                <div class="col-lg-4 col-md-6">
+                    <h5 class="h1" style="color: #FFC107">Welcome to MyCarrer</h5>
+                    <p class="small text-muted">We here at MyCarrer,Provide Resume and Roadmap to Building best Resume for your carrer. </p>
+                    <p class="small mb-0" style="color: #FFC107">&copy; Copyrights. All rights reserved.</p>
+                </div>
+                <div class="col-lg-2 col-md-6">
+                    <h5 class="text mb-3" style="color: #FFC107">Quick links</h5>
+                    <ul class="list-unstyled text-muted">
+                        <li><a href="#" class="text-white">Home</a></li>
+                        <li><a href="#" class="text-white">About</a></li>
+                        <li><a href="#" class="text-white">Get started</a></li>
+                        <li><a href="#" class="text-white">FAQ</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-2 col-md-6">
+                    <h5 class="text mb-3" style="color: #FFC107;">Quick links</h5>
+                    <ul class="list-unstyled text-muted">
+          
+                          <li> <a href="#" class="text-white"></a>Build Resume</li>
+                          <li> <a href="#" class="text-white">Look For Courses</a></li>
+                          <li> <a href="#" class="text-white">Help</a></li>
+                   
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <h5 class="text mb-3" style="color: #FFC107;">Hope you like it</h5>
+                    <p class="small text-muted">Subscribe and be the first to get the best deals and exciting offers on trending products.
+  
+                    </p>
+                    <form>
+                        <div class="input-group mb-3">
+                          <ul class="list-unstyled mb-0">
+                            <li><input class="form-control" type="text" placeholder="Username" aria-label="Username" aria-describedby="button-addon2"></li><br>
+                            <li><input class="form-control" type="password" placeholder="Password" aria-label="Password" aria-describedby="button-addon2"></li><br>
+                            <li><button type="button" class="btn btn-warning">Subscribe</button></li>
+                            </ul>
+                          </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        Copyright @ MyBus. All Rights Reserved  |  Contact Us: +91 90000 00000
+      
+    </footer>
